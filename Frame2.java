bf20ed87144c42cb85bfc49d1a765d468e4062ca
smile097575 <smile097575@gmail.com>
2018-10-04 11:16:02 +0800

    Add files via upload

---
+++ Frame2.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Frame2 extends JFrame{
+    public Frame2(){
+        ex2();
+    }
+    private void ex2(){
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setBounds(100,100,980,600);
+    }
+}
